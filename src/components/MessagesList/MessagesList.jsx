db635379ce353e3c19874bb538a5a99b0d4a223b
C1ndyy <cindx.xu@gmail.com>
2021-04-22 13:56:13 -0600

    added component files for message Panel

---
+++ src/components/MessagesList/MessagesList.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import MessageItem from "../MessageItem/MessageItem";
+
+const MessagesList = () => {
+  return (
+    <div>
+      <MessageItem />
+      <MessageItem />
+      <MessageItem />
+      <MessageItem />
+    </div>
+  );
+};
+
+export default MessagesList;
