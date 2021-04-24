f372867a31314d0c5825467ab31ca8bea296c89c
C1ndyy <cindx.xu@gmail.com>
2021-04-24 00:15:30 -0600

    set states on new group form

--- src/components/NewGroupForm/NewGroupForm.jsx
+++ src/components/NewGroupForm/NewGroupForm.jsx
@@ -1,15 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import "./NewGroupForm.css";
 
 function NewGroupForm() {
+  const [inputs, setInputs] = useState({
+    name: "",
+    category: "Other",
+    link: randomLink(),
+  });
+
+  function randomLink() {
+    return Math.random().toString(36).substr(2, 10);
+  }
+
   return (
     <div className="NewGroupForm">
       <h1>New Group</h1>
       <form autoComplete="off">
-        <input className="group-name" type="text" name="name" />
+        <input
+          className="group-name"
+          type="text"
+          name="name"
+          value={inputs.name}
+          onChange={(e) => setInputs({ ...inputs, name: e.target.value })}
+          required
+        />
         <label className="name-label">Group Name</label>
         <label>Category</label>
-        <select name="category">
+        <select
+          name="category"
+          value={inputs.category}
+          onChange={(e) => setInputs({ ...inputs, category: e.target.value })}
+        >
           <option>Family</option>
           <option>Friends</option>
           <option>Work</option>
@@ -19,8 +40,8 @@ function NewGroupForm() {
 
         <label>Invite Link</label>
         <div className="copy-link">
-          <input type="text" name="link" value="" />
-          <span class="material-icons">content_copy</span>
+          <input type="text" name="link" disabled value={inputs.link} />
+          <span className="material-icons">content_copy</span>
         </div>
         <button className="create-btn">Create Group</button>
       </form>
