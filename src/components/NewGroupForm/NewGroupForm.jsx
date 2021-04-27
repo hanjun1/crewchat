6fe71f14ac9d6e65c3ce2d0b585705547251507a
C1ndyy <cindx.xu@gmail.com>
2021-04-27 13:58:54 -0600

    UI for join group form

--- src/components/NewGroupForm/NewGroupForm.jsx
+++ src/components/NewGroupForm/NewGroupForm.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import "./NewGroupForm.css";
 import NewGroupSuccessModal from "../NewGroupSuccessModal/NewGroupSuccessModal";
+import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
+import "react-tabs/style/react-tabs.css";
 
 function NewGroupForm() {
   const [inputs, setInputs] = useState({
@@ -43,38 +45,54 @@ function NewGroupForm() {
 
   return (
     <div className="NewGroupForm">
-      <h1>New Group</h1>
-      <form autoComplete="off" onSubmit={handleSubmit}>
-        <input
-          className="group-name"
-          type="text"
-          name="name"
-          maxLength="25"
-          value={inputs.name}
-          onChange={(e) => setInputs({ ...inputs, name: e.target.value })}
-          required
-        />
-        <label className="name-label">Group Name</label>
-        <label>Category</label>
-        <select
-          name="category"
-          value={inputs.category}
-          onChange={(e) => setInputs({ ...inputs, category: e.target.value })}
-        >
-          <option>Family</option>
-          <option>Friends</option>
-          <option>Work</option>
-          <option>School</option>
-          <option>Other</option>
-        </select>
+      <Tabs>
+        <TabList>
+          <Tab>Create Group</Tab>
+          <Tab>Join Group</Tab>
+        </TabList>
 
-        {/* <label>Invite Link</label>
-        <div className="copy-link">
-          <input type="text" name="link" disabled value={inputs.link} />
-          <span className="material-icons">content_copy</span>
-        </div> */}
-        <button className="create-btn">Create Group</button>
-      </form>
+        <TabPanel>
+          <h1>New Group</h1>
+          <form autoComplete="off" onSubmit={handleSubmit}>
+            <input
+              className="group-name"
+              type="text"
+              name="name"
+              maxLength="25"
+              value={inputs.name}
+              onChange={(e) => setInputs({ ...inputs, name: e.target.value })}
+              required
+            />
+            <label className="name-label">Group Name</label>
+            <label>Category</label>
+            <select
+              name="category"
+              value={inputs.category}
+              onChange={(e) =>
+                setInputs({ ...inputs, category: e.target.value })
+              }
+            >
+              <option>Family</option>
+              <option>Friends</option>
+              <option>Work</option>
+              <option>School</option>
+              <option>Other</option>
+            </select>
+            <button className="create-btn">Create Group</button>
+          </form>
+        </TabPanel>
+        <TabPanel>
+          <h1 className="join-group-header">Join Group</h1>
+          <form autoComplete="off" onSubmit={handleSubmit}>
+            <label>Paste Invite Link Here</label>
+            <div className="copy-link">
+              <input type="text" name="link" />
+            </div>
+            <button className="create-btn">Join Group</button>
+          </form>
+        </TabPanel>
+      </Tabs>
+
       {showModal ? (
         <NewGroupSuccessModal
           link={inputs.link}
