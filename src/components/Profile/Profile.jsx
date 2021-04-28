0d39df45feb6284b31fb7a3049e88e5225075bbc
C1ndyy <cindx.xu@gmail.com>
2021-04-28 09:12:12 -0600

    edit profile rough UI

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -2,7 +2,21 @@ import React from "react";
 import "./Profile.css";
 
 function Profile() {
-  return <div className="Profile"></div>;
+  return (
+    <div className="Profile">
+      <h1>Edit My Profile</h1>
+      <div className="profile-pic">
+        <div className="edit-profile-pic">
+          <span class="material-icons md-light">edit</span>
+        </div>
+      </div>
+      <form autoComplete="off">
+        <label>Name</label>
+        <input type="text" name="name" required />
+        <button className="save-btn">Save</button>
+      </form>
+    </div>
+  );
 }
 
 export default Profile;
