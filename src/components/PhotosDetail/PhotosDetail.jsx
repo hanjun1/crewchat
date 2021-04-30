a5a4727a488a429bcd7227ba426ad9da0beb9504
Ryan Kim <hanjun1@ualberta.ca>
2021-04-29 21:18:34 -0600

    added ajax for images and files for chatdetails

--- src/components/PhotosDetail/PhotosDetail.jsx
+++ src/components/PhotosDetail/PhotosDetail.jsx
@@ -1,14 +1,40 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./PhotosDetail.css";
 
 function PhotosDetail(props) {
+  const [imgMsgs, setImgMsgs] = useState([]);
+
+  async function fetchMessage() {
+    try {
+      let jwt = localStorage.getItem("token");
+      const fetchResponse = await fetch(
+        `/api/groups/${props.activeGroup._id}`,
+        {
+          headers: { Authorization: "Bearer " + jwt },
+        }
+      );
+      let allMsgs = await fetchResponse.json();
+      let images = [];
+      for (let i = 0; i < allMsgs.length; i++) {
+        if (allMsgs[i].type === "image") {
+          images.push(allMsgs[i]);
+        }
+      }
+      setImgMsgs(images);
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  useEffect(() => {
+    fetchMessage();
+  }, [imgMsgs]);
+
   return (
     <div className="PhotosDetail">
-      {props.activeGroup.msgs
-        .filter((msg) => msg.type === "image")
-        .map((msg) => (
-          <img src={msg.image.imgFileURL} alt="IMG" />
-        ))}
+      {imgMsgs.map((msg) => (
+        <img src={msg.image.imgFileURL} alt="IMG" />
+      ))}
     </div>
   );
 }
